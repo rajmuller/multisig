b4bec3c4d1dd467f4d3070ad80e7468d84a098c7
adamRajmuller <rajfta@gmail.com>
2022-07-08 17:12:36 +0200

    feat: init wallet & test

--- programs/multisig/src/lib.rs
+++ programs/multisig/src/lib.rs
@@ -1,17 +1,84 @@
 use anchor_lang::prelude::*;
-use std::collections::HashMap;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("8XHSyugWk2uYagCREiD2fSRkgGcTPYvwipXgd9c7em2i");
 
 #[program]
 pub mod multisig {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    // init new multisig wallet with set of owners and threshold
+    pub fn init_wallet(
+        ctx: Context<InitWallet>,
+        owners: Vec<Pubkey>,
+        threshold: u64,
+        nonce: u8,
+    ) -> Result<()> {
+        assert_unique_owners(&owners)?;
+        require!(
+            threshold > 0 && threshold <= owners.len() as u64,
+            MultiSigError::InvalidThreshold
+        );
+        require!(!owners.is_empty(), MultiSigError::InvalidOwnersLen);
+
+        let multisig = &mut ctx.accounts.multisig;
+        multisig.owners = owners;
+        multisig.threshold = threshold;
+        multisig.nonce = nonce;
+        multisig.owner_set_seqno = 0;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct InitWallet<'info> {
+    #[account(zero, signer)]
+    multisig: Account<'info, MultiSig>,
+}
 
+#[account]
+pub struct MultiSig {
+    pub owners: Vec<Pubkey>,
+    pub threshold: u64,
+    pub nonce: u8,
+    pub owner_set_seqno: u32,
+}
+
+fn assert_unique_owners(owners: &[Pubkey]) -> Result<()> {
+    for (i, owner) in owners.iter().enumerate() {
+        require!(
+            !owners.iter().skip(i + 1).any(|item| item == owner),
+            MultiSigError::UniqueOwners
+        )
+    }
+    Ok(())
+}
+
+#[error_code]
+pub enum MultiSigError {
+    #[msg("The given owner is not part of this multisig.")]
+    InvalidOwner,
+
+    #[msg("Owners length must be non zero.")]
+    InvalidOwnersLen,
+
+    #[msg("Not enough owners signed this transaction.")]
+    NotEnoughSigners,
+
+    #[msg("Cannot delete a transaction that has been signed by an owner.")]
+    TransactionAlreadySigned,
+
+    #[msg("Overflow when adding.")]
+    Overflow,
+
+    #[msg("Cannot delete a transaction the owner did not create.")]
+    UnableToDelete,
+
+    #[msg("The given transaction has already been executed.")]
+    AlreadyExecuted,
+
+    #[msg("Threshold must be less than or equal to the number of owners.")]
+    InvalidThreshold,
+
+    #[msg("Owners must be unique")]
+    UniqueOwners,
+}
--- tests/multisig.ts
+++ tests/multisig.ts
@@ -1,5 +1,6 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
+import { expect } from "chai";
 import { Multisig } from "../target/types/multisig";
 
 describe("multisig", () => {
@@ -8,9 +9,49 @@ describe("multisig", () => {
 
   const program = anchor.workspace.Multisig as Program<Multisig>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  it("Should initialize!", async () => {
+    const multisigKeypair = anchor.web3.Keypair.generate();
+    // const [multisigSigner, nonce] =
+    //   await anchor.web3.PublicKey.findProgramAddress(
+    //     [multisigKeypair.publicKey.toBuffer()],
+    //     program.programId
+    //   );
+
+    const nonce = 10;
+    const multisigSize = 200; // Big enough.
+
+    const ownerAKeypair = anchor.web3.Keypair.generate();
+    const ownerBKeypair = anchor.web3.Keypair.generate();
+    const ownerCKeypair = anchor.web3.Keypair.generate();
+
+    const owners = [
+      ownerAKeypair.publicKey,
+      ownerBKeypair.publicKey,
+      ownerCKeypair.publicKey,
+    ];
+    const threshold = new anchor.BN(2);
+
+    await program.methods
+      .initWallet(owners, threshold, nonce)
+      .accounts({
+        multisig: multisigKeypair.publicKey,
+      })
+      .signers([multisigKeypair])
+      .preInstructions([
+        await program.account.multiSig.createInstruction(
+          multisigKeypair,
+          multisigSize
+        ),
+      ])
+      .rpc();
+
+    let multiSigState = await program.account.multiSig.fetch(
+      multisigKeypair.publicKey
+    );
+
+    expect(multiSigState.owners).to.eql(owners);
+    expect(multiSigState.nonce).to.eql(nonce);
+    expect(multiSigState.threshold.toString()).to.eql(threshold.toString());
+    expect(multiSigState.ownerSetSeqno).to.eql(0);
   });
 });
